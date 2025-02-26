6e82b21a70c643f27bfd002b98936248f2518d52
Mark Muthii <mark.muthii@strathmore.edu>
2025-02-26 03:16:57 +0300

    DB users

---
+++ server/src/db/models/user.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("user", userSchema);
+
+export { User };
