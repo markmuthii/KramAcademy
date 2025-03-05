d6b97580b6b79a1fa8955b61d383c5b0be4416c5
Mark Muthii <mark.muthii@strathmore.edu>
2025-03-06 02:09:34 +0300

    Add Shad

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div className="h-screen bg-black"></div>;
+  return (
+    <div className="h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Home;
