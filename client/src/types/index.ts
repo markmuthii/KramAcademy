de0e689b7c6d569df0aeb45bf86bcee37ec6efbc
Mark Muthii <mark.muthii@strathmore.edu>
2025-03-14 18:47:27 +0300

    Update types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -6,9 +6,19 @@ export type RegisterFormData = z.infer<typeof registerFormSchema>;
 export interface APIError {
   message: string;
   field?: string;
+  path?: string;
 }
 
 export interface APIErrorResponse {
   success: false;
   errors: APIError[];
+}
+
+export interface APIResponse<T> {
+  success: boolean;
+  data: T | T[];
+}
+
+export interface RegisterData {
+  message: string;
 }
