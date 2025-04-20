75b1b89ded3c93ca5e7fd8921b41fde94802f151
Mark Muthii <mark.muthii@strathmore.edu>
2025-04-20 15:59:46 +0300

    Update Auth logic

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,43 +1,67 @@
 "use server";
 
-import { fetapi } from "@/services/api";
+import { cookies, headers } from "next/headers";
+
 import { AuthData, LoginFormData, RegisterFormData } from "@/types";
+import { fetapi } from "@/services/api";
+import { createUserSession } from "@/lib/session";
 
-type RegisterState = string | null | { error: string };
-type LoginState = string | null | { error: string };
+type RegisterState = AuthData | null | { error: string };
+type LoginState = AuthData | null | { error: string };
 
-// TODO: Refactor this to use a single function for both login and register
-// and use a type guard to differentiate between the two states.
+export const authError = async (error: unknown) => {
+  return {
+    error:
+      error instanceof Error
+        ? error.message
+        : "An error occurred. Please try again.",
+  };
+};
+
+export const authHeaders = async () => {
+  return {
+    headers: Object.fromEntries((await headers()).entries()),
+  };
+};
+
 export const register = async (_: RegisterState, data: RegisterFormData) => {
   try {
-    const response = await fetapi.post<AuthData>("/auth/register", {
+    const result = await fetapi.post<AuthData>("/auth/register", {
       body: data,
     });
 
-    return response.message;
+    return result.data;
   } catch (error) {
-    return {
-      error:
-        error instanceof Error
-          ? error.message
-          : "An error occurred. Please try again.",
-    };
+    return await authError(error);
   }
 };
 
 export const login = async (_: LoginState, data: LoginFormData) => {
   try {
-    const response = await fetapi.post<AuthData>("/auth/login", {
+    const result = await fetapi.post<AuthData>("/auth/login", {
       body: data,
     });
 
-    return response.message;
+    // Create the user session by setting the cookie in the browser
+    await createUserSession(result.response);
+
+    return result.data;
   } catch (error) {
-    return {
-      error:
-        error instanceof Error
-          ? error.message
-          : "An error occurred. Please try again.",
-    };
+    return await authError(error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const result = await fetapi.post<AuthData>(
+      "/auth/logout",
+      await authHeaders()
+    );
+
+    // Clear the cookie in the browser
+    (await cookies()).delete("auth_token");
+    return result.data;
+  } catch (error) {
+    return await authError(error);
   }
 };
