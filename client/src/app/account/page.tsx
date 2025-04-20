c1186672b28345ffa2f06452d8a69db54ec2c975
Mark Muthii <mark.muthii@strathmore.edu>
2025-04-20 16:01:18 +0300

    Protect account page

--- client/src/app/account/page.tsx
+++ client/src/app/account/page.tsx
@@ -1,4 +1,13 @@
-const AccountPage = () => {
+import { getUserSession } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const AccountPage = async () => {
+  const session = await getUserSession();
+
+  if ("error" in session) {
+    redirect("/auth/login");
+  }
+
   return <div>AccountPage</div>;
 };
 
