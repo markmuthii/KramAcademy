3901226ee0cf2d6107ca7944c69356f18fcf34a3
Mark Muthii <mark.muthii@strathmore.edu>
2025-04-29 00:55:01 +0300

    Update metadata

--- client/src/app/auth/verify-email/page.tsx
+++ client/src/app/auth/verify-email/page.tsx
@@ -1,5 +1,11 @@
 import { VerifyEmail } from "@/components/auth/verify-email";
 
+export const metadata = {
+  title: "Verify Email - Thrifters",
+  description:
+    "Verify your email address to complete the registration process.",
+};
+
 const VerifyEmailPage = () => {
   return <VerifyEmail />;
 };
