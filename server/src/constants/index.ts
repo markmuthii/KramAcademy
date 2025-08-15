befb76d2cc83939b805f4095a88548dda48a4386
Mark Muthii <mark.muthii@strathmore.edu>
2025-08-15 14:11:57 +0300

    Update dynamic app name in server

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -4,7 +4,8 @@ export const APP_NAME = process.env.APP_NAME || "NExpress";
 export const ISPROD = process.env.NODE_ENV === "production";
 export const PORT = process.env.PORT || 3005;
 export const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/thrifters";
+  process.env.MONGO_URI ||
+  `mongodb://127.0.0.1:27017/${process.env.APP_NAME?.toLocaleLowerCase()}`;
 export const JWT_SECRET =
   process.env.JWT_SECRET || "extemelysecretkeygoeshereorintheenvfile";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
@@ -12,7 +13,8 @@ export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3005";
 export const PRODUCTION_DOMAIN = process.env.PRODUCTION_DOMAIN || "localhost";
 
 export const AUTH_COOKIE_NAME =
-  process.env.AUTH_COOKIE_NAME || "thrifters_auth";
+  process.env.AUTH_COOKIE_NAME ||
+  `${process.env.APP_NAME?.toLocaleLowerCase()}_auth`;
 export const COOKIE_OPTIONS = {
   httpOnly: true,
   maxAge: 1000 * 60 * 60 * 24,
